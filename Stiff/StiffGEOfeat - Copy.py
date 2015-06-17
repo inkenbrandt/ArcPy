7f7abe2a856146ca4994cf59584e6ce4a1f7d32e
inkenbrandt <paulinkenbrandt@utah.gov>
2015-06-17 08:25:26 -0600

    Fixed Check Box

--- Stiff/StiffGEOfeat - Copy.py
+++ Stiff/StiffGEOfeat - Copy.py
@@ -9,34 +9,29 @@
 
     @author: paulinkenbrandt
 """
-#Imports
+
+#from pylab import * 
 import numpy as np
 import os
 import arcpy
 from arcpy import env
+env.workspace = "CURRENT"
 
-env.workspace = "CURRENT" #Set workspace as open map
+#path = os.getcwd() 
 
-#Functions 
-def getfilename(path):
-    # this function extracts the file name without file path or extension
-    return path.split('\\').pop().split('/').pop().rsplit('.', 1)[0]
-
-
-#Inputs
 resultstable = arcpy.GetParameterAsText(0)
 meq = arcpy.GetParameterAsText(1)
 stationidfield = arcpy.GetParameterAsText(2)
-mv = int(arcpy.GetParameterAsText(3)) #vertical multiplier
-mh = int(arcpy.GetParameterAsText(4)) #horizontal multiplier
+vmultiplier = arcpy.GetParameterAsText(3)
+hmultiplier = arcpy.GetParameterAsText(4)
 fileplace = arcpy.GetParameterAsText(5)
+
+stationid, dt, Na, K, Ca, Mg, Cl, HCO3, CO3, SO4 = [],[],[],[],[],[],[],[],[],[]
 
 fieldnames = [u'Na', u'K', u'Ca', u'Mg', u'Cl', u'HCO3', u'CO3', u'SO4']
 
-fieldlist = [f.name for f in arcpy.ListFields(resultstable)] #get fields from input table
+fieldlist = [f.name for f in arcpy.ListFields(resultstable)]
 
-# Check to see if all parameters are in table; 
-# Create empty datasets for the missing parameters
 for field in fieldnames: #loop through each field
     if field not in fieldlist:
         arcpy.AddMessage(field + 'not one of the columns, adding field' )
@@ -48,10 +43,9 @@ for field in fieldnames: #loop through each field
 
 # Convert values in table fields to numpy arrays
 # populate lists with values from the active table
-StatId, Na, K, Ca, Mg, Cl, HCO3, CO3, SO4 = [],[],[],[],[],[],[],[],[]
 with arcpy.da.UpdateCursor(resultstable,[stationidfield, 'Na', 'K', 'Ca', 'Mg', 'Cl', 'HCO3', 'CO3', 'SO4']) as cursor:
     for row in cursor:
-        StatId.append(row[0])
+        stationid.append(row[0])
         Na.append(row[1])
         K.append(row[2])
         Ca.append(row[3])
@@ -61,24 +55,28 @@ with arcpy.da.UpdateCursor(resultstable,[stationidfield, 'Na', 'K', 'Ca', 'Mg', 
         CO3.append(row[7])
         SO4.append(row[8])
         
-
 x, y = [],[]
+
 with arcpy.da.SearchCursor(resultstable, "SHAPE@XY") as cursor:
     for row in cursor:
         x.append(row[0][0])
         y.append(row[0][1]) 
 
-# Get spatial reference of input dataset
+d = {'Ca':0.04990269, 'Mg':0.082287595, 'Na':0.043497608, 'K':0.02557656, 'Cl':0.028206596, 'HCO3':0.016388838, 'CO3':0.033328223, 'SO4':0.020833333, 'NO2':0.021736513, 'NO3':0.016129032}
+
+
+mv = float(vmultiplier) #vertical multiplier
+mh = float(hmultiplier) #horizontal multiplier
+
 desc = arcpy.Describe(resultstable)
 spatialref = desc.spatialReference 
 
 nosamp = len(Cl) # Determine number of samples in file
 
-#Conversion factors from mg/L to meq/L
-d = {'Ca':0.04990269, 'Mg':0.082287595, 'Na':0.043497608, 'K':0.02557656, 'Cl':0.028206596, 'HCO3':0.016388838, 'CO3':0.033328223, 'SO4':0.020833333, 'NO2':0.021736513, 'NO3':0.016129032}
+arcpy.AddMessage(meq)
 
-# Checkbox; If checked, meq/L conversion is done
 if str(meq) == 'true':
+
     # Column Index for parameters
     # Convert ion concentrations in mg/L to meq/L
     Cl = [Cl[i]*d['Cl'] for i in range(nosamp)]
@@ -93,7 +91,9 @@ if str(meq) == 'true':
 else:
     NaK = [Na[i]+K[i] for i in range(nosamp)]
 
-#Sum Anions and Cations to determine charge balance
+StatId = stationid
+
+
 Anions = [Cl[i]+HCO3[i]+CO3[i]+SO4[i] for i in range(nosamp)]
 Cations = [K[i]+Mg[i]+Na[i]+Ca[i] for i in range(nosamp)]
 EC = [Anions[i]+Cations[i] for i in range(nosamp)]
@@ -121,8 +121,6 @@ xy7 = [[xNaK[i], 1*10*mv + float(y[i]), 'NaK', NaK[i], StatId[i]] for i in range
 # list coordinate pairs to construct features
 feature_info = [[xy6[i],xy5[i],xy4[i],xy3[i],xy2[i],xy1[i]] for i in range(nosamp)]
 
-allpath = os.path.dirname(os.path.abspath(fileplace))
-
 #restructure arrays for a new table
 def unstack(stuff):
     x,y,par,conc,stat=[],[],[],[],[]    
@@ -146,11 +144,8 @@ axy,bxy,cxy,dxy,exy = [],[],[],[],[]
 varlist =[axy,bxy,cxy,dxy,exy]
 for i in range(len(a)):
     varlist[i] = np.append(a[i],(b[i],c[i],d[i],e[i],f[i]))
-varlist = list(zip(*np.array(varlist)))
-
-
+varlist = zip(*np.array(varlist))
 arcpy.AddMessage(str(len(varlist)) + " Features Plotted" )
-
 # denote table field names and types
 dts = {'names':('xfield', 'yfield', 'chemcode', 'conc', 'StationId'), 'formats': (np.float32, np.float32, '|S256', '|S256', '|S256')}
 
@@ -158,12 +153,15 @@ dts = {'names':('xfield', 'yfield', 'chemcode', 'conc', 'StationId'), 'formats':
 inarray = np.rec.fromrecords(varlist, dtype=dts)
 
 
-pointspath = allpath + '\\' + getfilename(fileplace) + "_points" + os.path.splitext(fileplace)[1]
+def getfilename(path):
+    # this function extracts the file name without file path or extension
+    return path.split('\\').pop().split('/').pop().rsplit('.', 1)[0]
 
 
-temptab = arcpy.env.scratchGDB + os.path.sep + "temptab12"
+pointspath = os.path.dirname(os.path.abspath(fileplace)) + '\\' + getfilename(fileplace) + "_points" + os.path.splitext(fileplace)[1]
+
+temptab = arcpy.env.scratchGDB + os.path.sep + "temptab6"
 templayer = arcpy.env.scratchGDB + os.path.sep + "templayers"
-
 
 arcpy.da.NumPyArrayToTable(inarray,temptab)
 arcpy.MakeXYEventLayer_management(temptab,"xfield","yfield",templayer,spatialref)
@@ -171,6 +169,9 @@ points = arcpy.CopyFeatures_management(templayer, pointspath)
 
 arcpy.Delete_management(temptab)
 arcpy.Delete_management(templayer)
+
+# get spatail ref from user
+
 
  
 # Create a Polygon object based on the array of points
@@ -236,7 +237,7 @@ barLabels = [[lab5[i], lab4[i], lab3[i], lab2[i], lab1[i], lab0[i], lab1n[i], la
 labelpointspath = os.path.dirname(os.path.abspath(fileplace))
 labelpointsname = getfilename(fileplace) + "_lbpnts" + os.path.splitext(fileplace)[1]
 
-sclpnts = arcpy.CreateFeatureclass_management(allpath,labelpointsname,"POINT",points, "DISABLED","DISABLED", spatialref)
+sclpnts = arcpy.CreateFeatureclass_management(labelpointspath,labelpointsname,"POINT",points, "DISABLED","DISABLED", spatialref)
 
 cursor = arcpy.da.InsertCursor(sclpnts,['SHAPE@X','SHAPE@Y','conc'])
 
@@ -253,7 +254,7 @@ df = arcpy.mapping.ListDataFrames(mxd)[0]
 
 layer = arcpy.mapping.Layer(linespath)
 layer1 = arcpy.mapping.Layer(pointspath)
-layer2 = arcpy.mapping.Layer(allpath+"\\"+labelpointsname)
+layer2 = arcpy.mapping.Layer(labelpointspath+"\\"+labelpointsname)
 arcpy.mapping.AddLayer(df, layer, "AUTO_ARRANGE")
 arcpy.mapping.AddLayer(df, layer1, "AUTO_ARRANGE")
 arcpy.mapping.AddLayer(df, layer2, "AUTO_ARRANGE")
